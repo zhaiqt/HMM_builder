56e72bce2e2c1a1d96274365419f4ea64e3ff6bc
zhaiqt <zhaiqt@hotmail.com>
2015-12-28 21:57:56 -0800

    process single protein sequence, output fragmented ab into FM and CDRs

--- cal_PMW_score.py
+++ cal_PMW_score.py
@@ -13,11 +13,12 @@ def find_high_PMW_score (PMW_dict,fragment):
 		stop= min(len(input_seq),motif_size)+1
 		frag_window=input_seq[0:stop]
 		score=0
+
 		# print "input_seq is " + input_seq
 		for i in range(motif_size):
 			score= score+ PMW_dict[str(i)][input_seq[i]]
 			#tmp_table=PMW_dict[str(i)]['E']
-		print frag_window +' : ' + str(score)
+		#print frag_window +' : ' + str(score)
 
 		if score <3: 
 			input_seq=input_seq[1:]
@@ -28,84 +29,101 @@ def find_high_PMW_score (PMW_dict,fragment):
 	return frag_window
 #--------------------------------find all the boundaries of given sequence-----------#
 def annotate_Ab_CDR (Ab_seq,species):
-	PMW_mFR1head_name='./data/'+species+'/FR1_head_PMW_'+species+'.json'
-	PMW_mFR1tail_name='./data/'+species+'/FR1_tail_PMW_'+species+'.json'
-	PMW_mFR2head_name='./data/'+species+'/FR2_head_PMW_'+species+'.json'
-	PMW_mFR2tail_name='./data/'+species+'/FR2_tail_PMW_'+species+'.json'
-	PMW_mFR3head_name='./data/'+species+'/FR3_head_PMW_'+species+'.json'
-	PMW_mFR3tail_name='./data/'+species+'/FR3_tail_PMW_'+species+'.json'
-	PMW_mFR4head_name='./data/'+species+'/FR4_head_PMW_'+species+'.json'
+	PMW_FR1head_name='./data/'+species+'/FR1_head_PMW_'+species+'.json'
+	PMW_FR1tail_name='./data/'+species+'/FR1_tail_PMW_'+species+'.json'
+	PMW_FR2head_name='./data/'+species+'/FR2_head_PMW_'+species+'.json'
+	PMW_FR2tail_name='./data/'+species+'/FR2_tail_PMW_'+species+'.json'
+	PMW_FR3head_name='./data/'+species+'/FR3_head_PMW_'+species+'.json'
+	PMW_FR3tail_name='./data/'+species+'/FR3_tail_PMW_'+species+'.json'
+	PMW_FR4head_name='./data/'+species+'/FR4_head_PMW_'+species+'.json'
 
 	
-	with open(PMW_mFR1head_name,'r') as fp:
-		PMW_mFR1head=json.load(fp)
-	with open(PMW_mFR1tail_name,'r') as fp:
-		PMW_mFR1tail=json.load(fp)
-	with open(PMW_mFR2head_name,'r') as fp:
-		PMW_mFR2head=json.load(fp)
-	with open(PMW_mFR2tail_name,'r') as fp:
-		PMW_mFR2tail=json.load(fp)
-	with open(PMW_mFR3head_name,'r') as fp:
-		PMW_mFR3head=json.load(fp)
-	with open(PMW_mFR3tail_name,'r') as fp:
-		PMW_mFR3tail=json.load(fp)
-	with open(PMW_mFR4head_name,'r') as fp:
-		PMW_mFR4head=json.load(fp)
+	with open(PMW_FR1head_name,'r') as fp:
+		PMW_FR1head=json.load(fp)
+	with open(PMW_FR1tail_name,'r') as fp:
+		PMW_FR1tail=json.load(fp)
+	with open(PMW_FR2head_name,'r') as fp:
+		PMW_FR2head=json.load(fp)
+	with open(PMW_FR2tail_name,'r') as fp:
+		PMW_FR2tail=json.load(fp)
+	with open(PMW_FR3head_name,'r') as fp:
+		PMW_FR3head=json.load(fp)
+	with open(PMW_FR3tail_name,'r') as fp:
+		PMW_FR3tail=json.load(fp)
+	with open(PMW_FR4head_name,'r') as fp:
+		PMW_FR4head=json.load(fp)
 
 	# test_string= "QVQLQQSGAELARPGASVKMSCKASGYTFTSYTMHWVKQRPGQGLEWIGYINPSSGYTKYNQKFKDKATLTADKSSSTAYMQLSSLTSEDSAVYYCAR"
+	#[FR1head_pos,FR1tail_pos,FR2head_pos,FR2tail_pos,FR3head_pos,FR3tail_pos,FR4head_pos]
 	try:
-		mFR1head=find_high_PMW_score(PMW_mFR1head,Ab_seq)
-		mFR1head_pos=Ab_seq.find(mFR1head)
-		print "mFR1head sequence starts at " + str(mFR1head_pos) +','+ mFR1head
+		FR1head=find_high_PMW_score(PMW_FR1head,Ab_seq)
+		FR1head_pos=Ab_seq.find(FR1head)
+		print "FR1head sequence starts at " + str(FR1head_pos) +','+ FR1head
 	except:
-		print "mFR1head can't be found in %s." % Ab_seq
+		print "FR1head can't be found in %s." % Ab_seq
 
 	try:
-		mFR1tail=find_high_PMW_score(PMW_mFR1tail,Ab_seq[mFR1head_pos:])
-		mFR1tail_pos=Ab_seq.find(mFR1tail)+len(mFR1tail)-1
-		print "mFR1head sequence ends at " + str(mFR1tail_pos) +','+ mFR1tail
+		FR1tail=find_high_PMW_score(PMW_FR1tail,Ab_seq[FR1head_pos:])
+		FR1tail_pos=Ab_seq.find(FR1tail)+len(FR1tail)-1
+		print "FR1head sequence ends at " + str(FR1tail_pos) +','+ FR1tail
 	except:
-		print "mFR1tail can't be found in %s." % Ab_seq
+		print "FR1tail can't be found in %s." % Ab_seq
 
 	try:
-		mFR2head=find_high_PMW_score(PMW_mFR2head,Ab_seq[mFR1tail_pos:])
-		mFR2head_pos=Ab_seq.find(mFR2head)
-		print "mFR2head sequence starts at " + str(mFR2head_pos) +','+ mFR2head
+		FR2head=find_high_PMW_score(PMW_FR2head,Ab_seq[FR1tail_pos:])
+		FR2head_pos=Ab_seq.find(FR2head)
+		print "FR2head sequence starts at " + str(FR2head_pos) +','+ FR2head
 	except:
-		print "mFR2head can't be found in %s." % Ab_seq
+		print "FR2head can't be found in %s." % Ab_seq
 
 	try:
-		mFR2tail=find_high_PMW_score(PMW_mFR2tail,Ab_seq[mFR2head_pos:])
-		mFR2tail_pos=Ab_seq.find(mFR2tail)+len(mFR2tail)-1
-		print "mFR1head sequence ends at " + str(mFR2tail_pos) +','+ mFR2tail
+		FR2tail=find_high_PMW_score(PMW_FR2tail,Ab_seq[FR2head_pos:])
+		FR2tail_pos=Ab_seq.find(FR2tail)+len(FR2tail)-1
+		print "FR2tail sequence ends at " + str(FR2tail_pos) +','+ FR2tail
 	except:
-		print "mFR2tail can't be found in %s." % Ab_seq
+		print "FR2tail can't be found in %s." % Ab_seq
 
 	try:
-		mFR3head=find_high_PMW_score(PMW_mFR3head,Ab_seq[mFR2tail_pos:])
-		mFR3head_pos=Ab_seq.find(mFR3head)
-		print "mFR3head sequence starts at " + str(mFR3head_pos) +','+ mFR2head
+		FR3head=find_high_PMW_score(PMW_FR3head,Ab_seq[FR2tail_pos:])
+		FR3head_pos=Ab_seq.find(FR3head)
+		print "FR3head sequence starts at " + str(FR3head_pos) +','+ FR3head
 	except:
-		print "mFR3head can't be found in %s." % Ab_seq
+		print "FR3head can't be found in %s." % Ab_seq
 
 	try:
-		mFR3tail=find_high_PMW_score(PMW_mFR3tail,Ab_seq[mFR3head_pos:])
-		mFR3tail_pos=Ab_seq.find(mFR3tail)+len(mFR3tail)-1
-		print "mFR3head sequence ends at " + str(mFR3tail_pos) +','+ mFR3tail
+		FR3tail=find_high_PMW_score(PMW_FR3tail,Ab_seq[FR3head_pos:])
+		FR3tail_pos=Ab_seq.find(FR3tail)+len(FR3tail)-1
+		print "FR3tail sequence ends at " + str(FR3tail_pos) +','+ FR3tail
 	except:
-		print "mFR3tail can't be found in %s." % Ab_seq
+		print "FR3tail can't be found in %s." % Ab_seq
 
 	try:
-		mFR4head=find_high_PMW_score(PMW_mFR4head,Ab_seq[mFR3tail_pos:])
-		mFR4head_pos=Ab_seq.find(mFR4head)
-		print "mFR4head sequence starts at " + str(mFR4head_pos) +','+ mFR4head
+		FR4head=find_high_PMW_score(PMW_FR4head,Ab_seq[FR3tail_pos:])
+		FR4head_pos=Ab_seq.find(FR4head)
+		print "FR4head sequence starts at " + str(FR4head_pos) +','+ FR4head
 	except:
-		print "mFR4head can't be found in %s." % Ab_seq
+		print "FR4head can't be found in %s." % Ab_seq
 
-	return
-#	for key in PMW_mFR4head:
+	FRM1=Ab_seq[FR1head_pos:FR1tail_pos+1]
+	CDR1=Ab_seq[FR1tail_pos+1:FR2head_pos]
+	FRM2=Ab_seq[FR2head_pos:FR2tail_pos+1]
+	CDR2=Ab_seq[FR2tail_pos+1:FR3head_pos]
+	FRM3=Ab_seq[FR3head_pos:FR3tail_pos+1]
+	CDR3=Ab_seq[FR3tail_pos+1:FR4head_pos]
+	# print FRM1 
+	# print CDR1
+	# print FRM2
+	# print CDR2
+	# print FRM3
+	# print CDR3
+	# boundary_list=[FR1head_pos,FR1tail_pos,FR2head_pos,FR2tail_pos,FR3head_pos,FR3tail_pos,FR4head_pos]
+	frag_list=[FRM1,CDR1,FRM2,CDR2,FRM3,CDR3]
+	print frag_list
+	#print boundary_list
+	return frag_list
+#	for key in PMW_FR4head:
 		# print "position " + str(key)
-		# print PMW_mFR4head[key]
+		# print PMW_FR4head[key]
 ##########  main ###############
 
 # generate a dictionary contain the HMM model
@@ -116,5 +134,4 @@ def annotate_Ab_CDR (Ab_seq,species):
 species="mouse"
 test_string="EVKLEESGAELVRPGASVTLSCAASGYTFTDFEIHWVKQPPVGGLEWIGTLDPETGGTAYNQNFKGRATLTADKSSSTAYMELRSLTSEDSAVYYCTRWGKKFYYYGTSYAMDYWGQGTSVTVSSAKTTPPSVYPLAPGXXATNSMVTLGCLVKGYFPEPVTVTWNSGSLSGGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVPRD"
 annotate_Ab_CDR (test_string,species)
-
 
